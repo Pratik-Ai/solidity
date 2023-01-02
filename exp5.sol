5bbdeba21e865880ce2930b17cafcd6401e72544
Pratik Pawar <76660388+Pratik-Ai@users.noreply.github.com>
2023-01-03 01:40:07 +0530

    Update exp5.sol

--- exp5.sol
+++ exp5.sol
@@ -1,1 +1,24 @@
-//
+pragma solidity > 0.5.0 < 0.9.0;
+
+contract wallet{
+    address payable public owner;
+
+    constructor() public
+    {
+        owner = payable(msg.sender);
+    }
+    function getmoney() public payable{}
+    // receive() external payable {}
+
+    function withdraw(uint _amount) external{
+        require(msg.sender == owner, "caller is not owner");
+
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalamce() external view returns (uint)
+    {
+        return address(this).balance;
+    }
+
+}
